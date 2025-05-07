c8cf3c2a7845341426e356858c8e7a4bba95efda
thaith <thaith@smartosc.com>
2025-05-07 10:22:49 +0700

    Update Day 8

--- D8/Ex3/main.py
+++ D8/Ex3/main.py
@@ -18,8 +18,11 @@ def main():
 
     window_spec = Window.partitionBy("region").orderBy(desc("total_quantity"))
     top_10_products = product_sales_by_region.withColumn("rank", row_number().over(window_spec)).filter(col("rank") <= 10)
-
     top_10_products.write.mode("overwrite").parquet("output/top_10_products_by_region.parquet")
+    
+    # --- print top_10_products.show() ---
+    print("Top 10 sản phẩm bán chạy theo vùng:")
+    top_10_products.show()
 
     # --- 2. Phân tích doanh thu theo tháng và danh mục ---
     sales_with_date = sales_joined.withColumn("month", month(to_date("date"))).withColumn("year", year(to_date("date")))
@@ -27,6 +30,10 @@ def main():
     monthly_revenue_trend = sales_with_date.groupBy("year", "month", "category").agg(sum("revenue").alias("total_revenue")).orderBy("year", "month", "category")
 
     monthly_revenue_trend.write.mode("overwrite").parquet("output/monthly_revenue_trend.parquet")
+    
+    # --- print monthly_revenue_trend.show() ---
+    print("Doanh thu theo tháng và danh mục:")
+    monthly_revenue_trend.show()
 
     # --- 3. Cửa hàng hiệu suất cao và thấp ---
     store_performance = sales_joined.groupBy("store_id", "store_name", "region").agg(sum("revenue").alias("total_revenue"))
@@ -36,6 +43,13 @@ def main():
 
     top_store.write.mode("overwrite").parquet("output/top_store.parquet")
     low_store.write.mode("overwrite").parquet("output/low_store.parquet")
+    
+    # --- print top_store.show() ---
+    print("Cửa hàng hiệu suất cao:")
+    top_store.show()
+    # --- print low_store.show() ---
+    print("Cửa hàng hiệu suất thấp:")
+    low_store.show()
 
     # spark.stop()
 
